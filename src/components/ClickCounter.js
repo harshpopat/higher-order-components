5f2c23d1317121299a4bcce4548e2db9c20197fa
Harsh Popat <harshpopat75@gmail.com>
2021-10-16 19:51:48 +0530

    files addded

---
+++ src/components/ClickCounter.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import withCounter from './withCounter';
+
+class ClickCounter extends Component {
+  render() {
+    const { count, incrementCount } = this.props;
+
+    return (
+      <div>
+        <button onClick={incrementCount}>Clicked {count} times</button>
+      </div>
+    );
+  }
+}
+
+export default withCounter(ClickCounter);
---
+++ src/components/withCounter.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const withCounter = (WrappedComponent) => {
+  class WithCounter extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        count: 0,
+      };
+    }
+
+    incrementCount = () => {
+      this.setState((prevState) => {
+        return { count: prevState.count + 1 };
+      });
+    };
+
+    render() {
+      return (
+        <WrappedComponent
+          count={this.state.count}
+          incrementCount={this.incrementCount}
+        />
+      );
+    }
+  }
+  return WithCounter;
+};
+
+export default withCounter;
